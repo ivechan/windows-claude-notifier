20f096c76ef30cec498995b5f58f49218b7eb852
ChenJingwen <lhchenjw@gmail.com>
2026-05-03 19:11:27 +0800

    add ip

--- test_curl.sh
+++ test_curl.sh
@@ -3,11 +3,11 @@
 TITLE=${1:-"测试通知"}
 BODY=${2:-"这是从 bash 脚本发送的测试消息"}
 
-echo "Sending curl POST request to http://127.0.0.1:5000/notify ..."
+echo "Sending curl POST request to http://192.168.3.18:5000/notify ..."
 echo "Title: $TITLE"
 echo "Body: $BODY"
 echo ""
 
-curl -X POST http://127.0.0.1:5000/notify -H "Content-Type: application/json" -d "{\"title\":\"$TITLE\",\"body\":\"$BODY\",\"sound\":\"default\"}"
+curl -X POST http://192.168.3.18:5000/notify -H "Content-Type: application/json" -d "{\"title\":\"$TITLE\",\"body\":\"$BODY\",\"sound\":\"default\"}"
 
 echo ""
